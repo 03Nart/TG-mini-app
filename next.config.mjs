d3f0991811b6f0b4771a6f0d324032a490425df2
Anania belay <77397808+ananiabelay@users.noreply.github.com>
2024-10-08 06:22:01 -0700

    this is just test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
 
-export default nextConfig;
+    images: {
+        domains: ['127.0.0.1', 'd.lazaristcatholicschool.org',], // All domains in one array
+    },
+  
+  };
+  
+  export default nextConfig;
+  
