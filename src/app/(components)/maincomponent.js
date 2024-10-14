06b0177b72661be0e4458a1d2c4ffb2dd00edd97
Anania Belay <hackeranania@gmail.com>
2024-10-14 19:34:31 +0300

    updating the ui for simpler use

--- src/app/(components)/maincomponent.js
+++ src/app/(components)/maincomponent.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Maincomponent = (params) => {
   return (
-    <a href={params.link} className="flex flex-col md:flex-row items-center bg-gray-900 rounded-lg shadow-lg transition transform hover:scale-105 hover:shadow-xl hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700 border-2 border-transparent hover:border-gray-600 p-4">
+    <a href={params.link} className="flex flex-row md:flex-row items-center bg-gray-900 rounded-lg shadow-lg transition transform hover:scale-105 hover:shadow-xl hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700 border-2 border-transparent hover:border-gray-600 p-4">
       <Image 
-        className="rounded-t-lg md:rounded-none md:rounded-s-lg object-cover h-96 md:h-auto md:w-48" 
+        className="rounded-t-lg md:rounded-none md:rounded-s-lg object-cover  h-auto w-48" 
         src={params.image}
         alt="Airdrop Image" 
         width={500}
