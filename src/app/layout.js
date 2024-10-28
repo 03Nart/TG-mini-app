4a481bea7ce7705d0d73593d3a2a9e6f4573c317
Anania Belay <hackeranania@gmail.com>
2024-10-28 13:16:28 +0300

    hidden

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
+        <script
+          src="https://telegram.org/js/telegram-web-app.js"
+          async
+        ></script>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 
       </head>
